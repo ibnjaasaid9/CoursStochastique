8673982948ea98948d2e5f47ab89e6f844a028b2
ibnjaasaid9 <saidibnjaa2023@gmail.com>
2024-07-15 07:23:41 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Animation du titre
     const title = document.querySelector('.header-left h1');
-    if (title) {
-        title.style.animation = 'slideIn 2s ease-in-out';
-    }
+    title.style.animation = 'slideIn 2s ease-in-out';
 
     // Animation des images de livres
     const books = document.querySelectorAll('.book-references .book img');
@@ -15,22 +13,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
         book.addEventListener('mouseout', function() {
             this.style.transform = 'scale(1)';
-        });
-    });
-
-    // Boutons Objectifs et QCM
-    document.querySelectorAll('.objectives-button').forEach(button => {
-        button.addEventListener('click', function() {
-            const chapterContent = this.parentElement;
-            const objectivesList = chapterContent.querySelector('.objectives-list');
-            objectivesList.classList.toggle('hidden');
-        });
-    });
-
-    // Boutons QCM
-    document.querySelectorAll('.qcm-button').forEach(button => {
-        button.addEventListener('click', function() {
-            alert('QCM pour ' + this.parentElement.parentElement.getAttribute('data-chapter'));
         });
     });
 
